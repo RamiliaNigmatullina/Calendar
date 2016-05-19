34249f9443b851f36685fe476bbcc8eafeaa8659
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2016-05-19 20:32:21 +0300

    first_name and second_name added to user

--- Calendar/db/schema.rb
+++ Calendar/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510142550) do
+ActiveRecord::Schema.define(version: 20160519073045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20160510142550) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "second_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
